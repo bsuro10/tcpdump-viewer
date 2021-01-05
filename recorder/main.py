37f556a3a6f817946166e97cf44ebf43dd4b9dbb
Ran Biderman <bsuro10@gmail.com>
2021-01-05 10:39:27 +0200

    findProcessById finished

--- recorder/main.py
+++ recorder/main.py
@@ -6,23 +6,22 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def handler():
     timeout = request.args.get('timeout') or '1'
-    processid = request.args['processid']
+    processid = findProcessId(request.args['containerid'])
     port = request.args.get('port') or ''
     host = request.args['host']
-
+    
     proc = subprocess.Popen(['timeout',timeout + 's','./record.sh','--processid=' + processid,'--port=' + port, '--host=' + host],stderr=subprocess.PIPE)
     err = proc.communicate()[0]
 
     if err:
         return ('An error has occurred while running the script ./record.sh', 500)
 
-    return ('Succeded',200)
+    return (processid,200)
 
 def findProcessId(containerid):
-    proc = subprocess.Popen(['docker','inspect','-f','{{.State.Pid}}', containerid], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    processid = proc.communicate()[0]
-    #err = proc.communicate()[1]
+    proc = subprocess.Popen(['podman','inspect','-f','{{.State.Pid}}', containerid], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    processId, err = proc.communicate()
     
-    #if err:
-    #    return err
-    return 
+    if err:
+        return err
+    return processId
